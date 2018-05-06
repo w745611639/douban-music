17b2126a97e0b70ae16832e779bf7be5fc326e42
wang <745611639@qq.com>
2018-05-06 21:49:44 +0800

    pages1

--- src/js/getSearchMsg.js
+++
@@ -1,9 +0,0 @@
-(function ($, root) {
-	let msgObj = {};
-	//  获取url的搜索信息 key=...&id=....
-	let msgArr = decodeURI(window.location.search).slice(1).split("&").map(ele => ele.split('='));
-	msgArr.forEach(ele => {
-		msgObj[ele[0]] = ele[1];
-	});
-	root.msgObj = msgObj;
-}(window.jquery, window.musicManager || (window.musicManager = {})))
